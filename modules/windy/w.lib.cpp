02304d5ddae7b028beee57db71f8d64d321f4ded
Windy Darian <p123456638@msn.com>
2019-09-10 21:55:41 -0400

    spatial_set_rotation_quat by default resets scale. This make things simpler.

--- modules/windy/w.lib.cpp
+++ modules/windy/w.lib.cpp
@@ -5,17 +5,29 @@
 
 _W* _W::singleton = nullptr;
 
-static void _spatial_set_rotation_quat(Spatial *spatial, const Quat &quat)
+static void _spatial_set_rotation_quat(Spatial *spatial, const Quat &rotation)
 {
 	ERR_FAIL_NULL(spatial);
-	Transform transform = spatial->get_transform();  // TODO: can I avoid copying here???
-	Vector3 original_scale(transform.basis.get_scale());
-	transform.set_basis(Basis(quat, original_scale));
+	Transform transform = spatial->get_transform();
+	transform.set_basis(Basis(rotation));
 	spatial->set_transform(transform);
 }
-void _W::spatial_set_rotation_quat(Node *spatial, const Quat &quat) const   // Note: Spatial doesn't have conversion to Variant...
+void _W::spatial_set_rotation_quat(Node *spatial, const Quat &rotation) const   // Note: Spatial doesn't have conversion to Variant...
 {
-	_spatial_set_rotation_quat(Object::cast_to<Spatial>(spatial), quat);
+	_spatial_set_rotation_quat(Object::cast_to<Spatial>(spatial), rotation);
+}
+
+static void _spatial_set_rotation_quat_keep_scale(Spatial *spatial, const Quat &rotation)
+{
+	ERR_FAIL_NULL(spatial);
+	Transform transform = spatial->get_transform();
+	Vector3 original_scale(transform.basis.get_scale());
+	transform.set_basis(Basis(rotation, original_scale));
+	spatial->set_transform(transform);
+}
+void _W::spatial_set_rotation_quat_keep_scale(Node* spatial, const Quat& rotation) const
+{
+	_spatial_set_rotation_quat_keep_scale(Object::cast_to<Spatial>(spatial), rotation);
 }
 
 static Quat _spatial_get_rotation_quat(const Spatial *spatial)
@@ -23,7 +35,6 @@ static Quat _spatial_get_rotation_quat(const Spatial *spatial)
 	ERR_FAIL_NULL_V(spatial, Quat());
 	return spatial->get_transform().basis.get_rotation_quat();
 }
-
 Quat _W::spatial_get_rotation_quat(const Node *spatial) const
 {
 	return _spatial_get_rotation_quat(Object::cast_to<Spatial>(spatial));
@@ -31,6 +42,7 @@ Quat _W::spatial_get_rotation_quat(const Node *spatial) const
 
 void _W::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("spatial_set_rotation_quat", "spatial", "quat"), &_W::spatial_set_rotation_quat);
+	ClassDB::bind_method(D_METHOD("spatial_set_rotation_quat_keep_scale", "spatial", "quat"), &_W::spatial_set_rotation_quat_keep_scale);
 	ClassDB::bind_method(D_METHOD("spatial_get_rotation_quat", "spatial"), &_W::spatial_get_rotation_quat);
 }
 
--- modules/windy/w.lib.h
+++ modules/windy/w.lib.h
@@ -15,8 +15,12 @@ protected:
 	static void _bind_methods();
 
 public:
+	/** Get local rotation of a spatial node as a quaternion. */
 	Quat spatial_get_rotation_quat(const Node* spatial) const;
-	void spatial_set_rotation_quat(Node* spatial, const Quat& rotation_quat) const;
+	/** Set local rotation of a spatial node from a quaternion. This will reset local scale to one. */
+	void spatial_set_rotation_quat(Node* spatial, const Quat& rotation) const;
+	/** Set local rotation of a spatial node from a quaternion. This will keep local scale. */
+	void spatial_set_rotation_quat_keep_scale(Node* spatial, const Quat& rotation) const;
 
 	static _W* get_singleton() {return singleton;};
 	_W();
