f649243e040d70661e9c3bda17368f1c8e9431b9
Windy Darian <p123456638@msn.com>
2019-10-14 23:08:39 -0400

    have my module being a submodule (hmmm) at https://github.com/WindyDarian/godot_module_w

--- modules/windy/register_types.h
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2019 Windy Darian, all rights reserved.
-// Custom script library for things.
-
-#pragma once
-
-void register_windy_types();  // middle word must match module name
-void unregister_windy_types();
