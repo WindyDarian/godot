47a0620014e01ddaac70f98b5757b3527031a107
Windy Darian <WindyDarian@users.noreply.github.com>
2019-12-04 22:26:44 -0500

    Use Visual studio 2019 and 32 threads because why not

--- generate_vs_project_files.bat
+++ generate_vs_project_files.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64 && cd %cd% && scons p=windows vsproj=yes -j6
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64 && cd %cd% && scons p=windows vsproj=yes -j32
