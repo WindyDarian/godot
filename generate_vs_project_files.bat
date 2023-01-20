7ec9ccd204606075646d787fb6e721aeedd3dda9
Windy Darian <WindyDarian@users.noreply.github.com>
2023-01-20 08:31:06 -0500

    double
    it solves open world problem. I think it's worth it.

--- generate_vs_project_files.bat
+++ generate_vs_project_files.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64 && cd %cd% && scons p=windows vsproj=yes dev_build=true -j30
+"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64 && cd %cd% && scons p=windows vsproj=yes dev_build=true precision=double -j30
