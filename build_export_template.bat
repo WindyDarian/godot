ba98ee8dbffd16af7d0c0c6388e2c632d35ed207
Windy Darian <WindyDarian@users.noreply.github.com>
2024-12-11 21:40:57 -0500

    Update build_export_template.bat

--- build_export_template.bat
+++ build_export_template.bat
@@ -1,1 +1,2 @@
 "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64 && cd %cd% && scons platform=windows tools=no target=template_debug  precision=double -j32
+"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64 && cd %cd% && scons platform=windows tools=no target=template_release  precision=double -j32
